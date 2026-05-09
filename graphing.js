42530eb258295110f52049d066334bcce67845dc
Samuel <sami6543pz@gmail.com>
2026-05-09 16:46:45 -0500

    Implemented basic panning for the canvas panel

--- graphing.js
+++ graphing.js
@@ -1,27 +1,97 @@
 const panelContainer = document.getElementById('graphing-panel');
 const canvas = document.getElementById('my-canvas');
 const ctx = canvas.getContext('2d');
-const step = 40;
 
 canvas.width = panelContainer.offsetWidth;
 canvas.height = panelContainer.offsetHeight;
 
-ctx.beginPath();
-ctx.lineWidth = 1;
-ctx.strokeStyle = '#171717';
+const viewportTransform = {
+  x: 0,
+  y: 0,
+  scale: 1
+};
 
-//draw Vertical Lines
-
-for (let x = 0; x <= canvas.width; x += step) {
-  ctx.moveTo(x, 0);
-  ctx.lineTo(x, canvas.clientHeight);
+const drawRect = (x, y, width, height, color) => {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, width, height);
 }
 
-//draw Horizontal Lines
+//keep track of previous mouse position for later
+let previousX = 0;
+let previousY = 0;
 
-for (let y = 0; y <= canvas.height; y+= step) {
-  ctx.moveTo(0, y);
-  ctx.lineTo(canvas.clientWidth, y);
+const updatePanning = (e) => {
+  //set current x and y coordinates
+  const localX = e.clientX;
+  const localY = e.clientY;
+
+  //move viewport by the difference between current
+  // coordinates and previous coordinates
+  viewportTransform.x += localX - previousX;
+  viewportTransform.y += localY - previousY;
+
+  //update previous coordinates for next time
+  previousX = localX;
+  previousY = localY;
 }
 
-ctx.stroke();
+const render = () => {
+
+  ctx.setTransform(1, 0, 0, 1, 0, 0);
+  ctx.clearRect(0,0, canvas.width, canvas.height);
+  ctx.setTransform(
+    viewportTransform.scale,
+    0,
+    0,
+    viewportTransform.scale,
+    viewportTransform.x,
+    viewportTransform.y
+  );
+
+  drawRect(0, 50, 100, 100, 'red');
+  drawRect(200, 200, 100, 100, 'blue');
+}
+
+const onMouseMove = (e) => {
+  updatePanning(e);
+  render();
+  console.log(e);
+}
+
+canvas.addEventListener('mousedown', (e) => {
+  previousX = e.clientX;
+  previousY = e.clientY;
+
+  canvas.addEventListener('mousemove', onMouseMove);
+})
+
+canvas.addEventListener('mouseup', (e) => {
+  canvas.removeEventListener('mousemove', onMouseMove);
+})
+
+
+// const step = 30;
+
+// ctx.beginPath();
+// ctx.lineWidth = 0.5;
+// ctx.strokeStyle = '#9e9e9e';
+
+// //draw Vertical Lines
+
+// for (let x = 0.5; x <= canvas.width; x += step) {
+//   ctx.moveTo(x, 0);
+//   ctx.lineTo(x, canvas.clientHeight);
+// }
+
+// //draw Horizontal Lines
+
+// for (let y = 0.5; y <= canvas.height; y+= step) {
+//   ctx.moveTo(0, y);
+//   ctx.lineTo(canvas.clientWidth, y);
+// }
+
+// ctx.stroke();
+
+
+
+
