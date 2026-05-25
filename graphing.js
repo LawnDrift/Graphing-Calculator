ef890a67beb2667910ba6259c3a9899b2e2754e4
Samuel <sami6543pz@gmail.com>
2026-05-24 20:00:01 -0500

    fixed zooming by drawing grid units starting from corresponding y-axis and x-axis centered at the screen.

--- graphing.js
+++ graphing.js
@@ -4,7 +4,7 @@ const ctx = canvas.getContext('2d');
 
 canvas.width = panelContainer.offsetWidth;
 canvas.height = panelContainer.offsetHeight;
-const BASE_STEP = 20;
+const BASE_STEP = 40;
 
 //update panelContainer whenever window is resized
 window.addEventListener('resize', () => {
@@ -22,45 +22,33 @@ const viewportTransform = {
 
 
 const drawGrid = () => {
-  //square size for each square in grid
-  let step = BASE_STEP * viewportTransform.scale;
+  // horizontal and vertical center on the screen
+  const centerH = viewportTransform.x + canvas.width/2;
+  const centerV = viewportTransform.y + canvas.height/2;
   
-  const halfWidth = Math.round(canvas.width / 2);
-  const halfHeight = Math.round(canvas.height / 2);
-  step = halfWidth / Math.round(halfWidth/step);
+  //square size for each square in grid, changes with zooming
+  const step = BASE_STEP * viewportTransform.scale;
+  
   ctx.beginPath();
   ctx.lineWidth = 0.5;
   ctx.strokeStyle = '#9e9e9e';
   ctx.textAlign = "right";
   ctx.font = "14px Arial";
 
-  //establish offset based on the new viewport
-  const offsetX = viewportTransform.x % step;
-  const offsetY = viewportTransform.y % step;
-
-  // True center origins of the canvas y and x axis
-  // origin moves away as you scroll farther through panning
-  // returns horizontal and vertical distances from the center
-  const centerH = viewportTransform.x + step * (Math.round((canvas.width / step)/2));
-  const centerV = viewportTransform.y + step * (Math.round((canvas.height / step)/2));
-  
-  console.log(centerH);
-  console.log(centerV);
-  console.log(canvas.width/2);
-  console.log(canvas.height/2);
+ 
   console.log('------');
-  //draw vertical lines through each unit visible on screen
-  for (let x = offsetX + 0.5; x <= canvas.width; x += step) {
+  // Vertical lines to the right of y-axis
+  for (let x = centerH; x <= canvas.width; x += step) {
     //draw the vertical lines that cover screen
-    ctx.moveTo(x, 0);
-    ctx.lineTo(x, canvas.height);
+    ctx.moveTo(x+0.5, 0);
+    ctx.lineTo(x+0.5, canvas.height);
     
     //draw current coordinate in units
     // "x" is the line position inside the canvas
     // "centerH" is the distance from the origin
     // "x - centerH" gives how many pixels away is this line from zero
     // dividing by step returns that in units
-    let currentNum = Math.round((x - centerH) / step);
+    const currentNum = Math.round((x - centerH) / step);
     //only show even numbers
     if (currentNum % 2 == 0) {
       //show x-coord 0 behind the y-axis
@@ -73,30 +61,64 @@ const drawGrid = () => {
       
     }
   
+  }
 
+  // Vertical lines to the left of y-axis
+  for (let x = centerH - step; x >= 0; x -= step) {
+    //draw the vertical lines that cover screen
+    ctx.moveTo(x+0.5, 0);
+    ctx.lineTo(x+0.5, canvas.height);
+    
+    //draw current coordinate in units
+    // "x" is the line position inside the canvas
+    // "centerH" is the distance from the origin
+    // "x - centerH" gives how many pixels away is this line from zero
+    // dividing by step returns that in units
+    const currentNum = Math.round((x - centerH) / step);
+    //only show even numbers
+    if (currentNum % 2 == 0) {
+      ctx.fillText(currentNum, x, centerV+step/2);
+    }
+  
   }
 
 
-  //draw Horizontal Lines through each unit visible on screen
-  for (let y = offsetY + 0.5; y <= canvas.height; y+= step) {
-    ctx.moveTo(0, y);
-    ctx.lineTo(canvas.width, y);
+  //Horizontal lines below x-axis
+  for (let y = centerV; y <= canvas.height; y += step) {
+    ctx.moveTo(0, y+0.5);
+    ctx.lineTo(canvas.width, y+0.5);
 
     //draw current coordinate in units
     // "y" is the line position inside the canvas
     // "centerV" is the distance from the origin
     // "y - centerV" gives how many pixels away is this line from zero
     // dividing by step returns that in units
-    let currentNum = -1*Math.round((y - centerV) / step);
+    const currentNum = -1*Math.round((y - centerV) / step);
 
     //only show even numbers
     if (currentNum % 2 == 0 && currentNum != 0) {
         ctx.fillText(currentNum, centerH-step/5, y+step/4);
-      
-      
     }
   }
   
+  //Horizontal lines above x-axis
+  for (let y = centerV - step; y >= 0; y -= step) {
+    ctx.moveTo(0, y+0.5);
+    ctx.lineTo(canvas.width, y+0.5);
+
+    //draw current coordinate in units
+    // "y" is the line position inside the canvas
+    // "centerV" is the distance from the origin
+    // "y - centerV" gives how many pixels away is this line from zero
+    // dividing by step returns that in units
+    const currentNum = -1*Math.round((y - centerV) / step);
+
+    //only show even numbers
+    if (currentNum % 2 == 0 && currentNum != 0) {
+        ctx.fillText(currentNum, centerH-step/5, y+step/4);
+    }
+  }
+
   ctx.stroke();
 
   drawCenterLines(centerH, centerV);
