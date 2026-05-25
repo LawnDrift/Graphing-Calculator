72cc43fc9e1dee92fc1cc1d536ecc169f5935966
Samuel <sami6543pz@gmail.com>
2026-05-24 20:21:23 -0500

    organized code by creating function for drawing the coordinates as opposed to doing it inside the drawGrid() function

--- graphing.js
+++ graphing.js
@@ -32,8 +32,7 @@ const drawGrid = () => {
   ctx.beginPath();
   ctx.lineWidth = 0.5;
   ctx.strokeStyle = '#9e9e9e';
-  ctx.textAlign = "right";
-  ctx.font = "14px Arial";
+  
 
  
   console.log('------');
@@ -42,13 +41,50 @@ const drawGrid = () => {
     //draw the vertical lines that cover screen
     ctx.moveTo(x+0.5, 0);
     ctx.lineTo(x+0.5, canvas.height);
-    
+  }
+
+  // Vertical lines to the left of y-axis
+  for (let x = centerH - step; x >= 0; x -= step) {
+    //draw the vertical lines that cover screen
+    ctx.moveTo(x+0.5, 0);
+    ctx.lineTo(x+0.5, canvas.height);
+  }
+
+
+  //Horizontal lines below x-axis
+  for (let y = centerV; y <= canvas.height; y += step) {
+    ctx.moveTo(0, y+0.5);
+    ctx.lineTo(canvas.width, y+0.5);
+  }
+  
+  //Horizontal lines above x-axis
+  for (let y = centerV - step; y >= 0; y -= step) {
+    ctx.moveTo(0, y+0.5);
+    ctx.lineTo(canvas.width, y+0.5);
+  }
+
+  ctx.stroke();
+  
+  drawCenterLines(centerH, centerV);
+  drawCoordinates(centerH, centerV, step);
+}
+
+const drawCoordinates = (centerH, centerV, step) => {
+  ctx.beginPath();
+  ctx.textAlign = "right";
+  ctx.font = "14px Arial";
+  ctx.fillStyle = "black";
+
+  const numScale = Math.round(viewportTransform.scale);
+  
+  // Vertical lines to the right of y-axis
+  for (let x = centerH; x <= canvas.width; x += step) {
     //draw current coordinate in units
     // "x" is the line position inside the canvas
     // "centerH" is the distance from the origin
     // "x - centerH" gives how many pixels away is this line from zero
     // dividing by step returns that in units
-    const currentNum = Math.round((x - centerH) / step);
+    const currentNum = Math.round((x - centerH) / step) * numScale;
     //only show even numbers
     if (currentNum % 2 == 0) {
       //show x-coord 0 behind the y-axis
@@ -58,71 +94,39 @@ const drawGrid = () => {
       else {
         ctx.fillText(currentNum, x, centerV+step/2);
       }
-      
     }
-  
   }
-
   // Vertical lines to the left of y-axis
   for (let x = centerH - step; x >= 0; x -= step) {
-    //draw the vertical lines that cover screen
-    ctx.moveTo(x+0.5, 0);
-    ctx.lineTo(x+0.5, canvas.height);
-    
-    //draw current coordinate in units
-    // "x" is the line position inside the canvas
-    // "centerH" is the distance from the origin
-    // "x - centerH" gives how many pixels away is this line from zero
-    // dividing by step returns that in units
-    const currentNum = Math.round((x - centerH) / step);
-    //only show even numbers
+    const currentNum = Math.round((x - centerH) / step) * numScale;
     if (currentNum % 2 == 0) {
       ctx.fillText(currentNum, x, centerV+step/2);
     }
-  
   }
 
-
-  //Horizontal lines below x-axis
+  // Horizontal lines below x-axis
   for (let y = centerV; y <= canvas.height; y += step) {
-    ctx.moveTo(0, y+0.5);
-    ctx.lineTo(canvas.width, y+0.5);
-
     //draw current coordinate in units
     // "y" is the line position inside the canvas
     // "centerV" is the distance from the origin
     // "y - centerV" gives how many pixels away is this line from zero
     // dividing by step returns that in units
-    const currentNum = -1*Math.round((y - centerV) / step);
-
+    const currentNum = -1*Math.round((y - centerV) / step) * numScale;
     //only show even numbers
     if (currentNum % 2 == 0 && currentNum != 0) {
         ctx.fillText(currentNum, centerH-step/5, y+step/4);
     }
   }
-  
+
   //Horizontal lines above x-axis
   for (let y = centerV - step; y >= 0; y -= step) {
-    ctx.moveTo(0, y+0.5);
-    ctx.lineTo(canvas.width, y+0.5);
-
-    //draw current coordinate in units
-    // "y" is the line position inside the canvas
-    // "centerV" is the distance from the origin
-    // "y - centerV" gives how many pixels away is this line from zero
-    // dividing by step returns that in units
-    const currentNum = -1*Math.round((y - centerV) / step);
-
-    //only show even numbers
+    const currentNum = -1*Math.round((y - centerV) / step) * numScale;
     if (currentNum % 2 == 0 && currentNum != 0) {
         ctx.fillText(currentNum, centerH-step/5, y+step/4);
     }
   }
 
   ctx.stroke();
-
-  drawCenterLines(centerH, centerV);
-
 }
 
 const drawCenterLines = (centerH, centerV) => {
