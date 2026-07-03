40aa15fbdca7b39c37c91ee60e922da6e53364f8
Samuel <sami6543pz@gmail.com>
2026-07-03 00:04:26 -0500

    Added basic event listener for input field

--- input.js
+++ input.js
@@ -1,3 +1,6 @@
+const inputEquation= document.querySelector('.equation');
+
+
 export const linearFunc = (x) => {
   return x;
 }
@@ -6,4 +9,14 @@ export const quadraticFunc = (x) => {
   return x*x;
 }
 
+export const userEquation = (inputVal) => {
+  if (inputVal === "y=x") {
+    console.log('good, it works!');
+    
+  }
+}
 
+inputEquation.addEventListener("input", (e) => {
+
+  userEquation(e.target.value);
+});
